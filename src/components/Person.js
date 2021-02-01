0a0ed7a92410be2c50a338bdc8716293b206e570
LeKhiem <LeKhiem@LEHOANGKHIEM>
2021-02-01 19:50:05 +0700

    Xuất nội dung động cho component

---
+++ src/components/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Person = () => {
+    return (
+        <>
+        <p>This is a person.</p>
+        <p>I'm {Math.floor(Math.random()*30)} years old.</p>
+        </>
+    )
+
+}
+export default Person;
